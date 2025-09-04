fd8ba0da664079fa45c388d736a3a2533f7636a7
PatrickGura <julian.tatoy@urios.edu.ph>
2025-09-04 10:50:28 +0800

    new

--- database/migrations/2025_09_04_023639_create_student_table.php
+++ database/migrations/2025_09_04_023639_create_student_table.php
@@ -15,7 +15,7 @@ class CreateStudentTable extends Migration
     {
         Schema::create('student', function (Blueprint $table) {
             $table->id();
-            $table->string('tatoy');
+            $table->string('tatoyjulian');
             $table->timestamps();
         });
     }
